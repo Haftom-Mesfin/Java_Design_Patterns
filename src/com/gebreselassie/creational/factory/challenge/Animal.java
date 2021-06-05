72a502cfed7cd33ed8be0596284761d600fad182
Haftom-Mesfin <sadtime1906@gmail.com>
2021-06-05 13:33:36 -0500

    challenge factory method

---
+++ src/com/gebreselassie/creational/factory/challenge/Animal.java
@@ -0,0 +1,5 @@
+package com.gebreselassie.creational.factory.challenge;
+
+public interface Animal {
+    void eat();
+}
---
+++ src/com/gebreselassie/creational/factory/challenge/Tiger.java
@@ -0,0 +1,8 @@
+package com.gebreselassie.creational.factory.challenge;
+
+public class Tiger implements Animal{
+    @Override
+    public void eat() {
+        System.out.println("Tiger eats deer.");
+    }
+}
