e32c239d7c42804b40042050fbf10afd1740f2c5
Haftom-Mesfin <sadtime1906@gmail.com>
2021-06-05 21:05:28 -0500

    Added Lion

---
+++ src/com/gebreselassie/creational/factory/challenge/Lion.java
@@ -0,0 +1,8 @@
+package com.gebreselassie.creational.factory.challenge;
+
+public class Lion implements Animal{
+    @Override
+    public void eat() {
+        System.out.println("Lion eats a buffalo.");
+    }
+}
